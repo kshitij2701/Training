2371706cf6f8c20ced632c0901209879dd216176
kshitij2701 <kshitij.20b0101046@abes.ac.in>
2022-11-13 20:28:32 +0530

    mui- class 13

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import {
-    TextField, Grid, FormGroup, FormControlLabel, FormLabel,
-    Checkbox, RadioGroup, Radio, FormControl
+    TextField,
+    Grid,
+    FormGroup,
+    FormControlLabel,
+    FormLabel,
+    Checkbox,
+    RadioGroup,
+    Radio,
+    FormControl,
+    Select,
+    InputLabel,
+    MenuItem,
+    Switch
 } from '@mui/material';
+
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 import SportsCricketOutlinedIcon from '@mui/icons-material/SportsCricketOutlined';
 import SportsCricketIcon from '@mui/icons-material/SportsCricket';
 
@@ -20,7 +34,8 @@ function Signup() {
     // const [username, setUserName] = React.useState("");
     const [textData, setTextData] = React.useState({
         username: "",
-        about:""
+        about: "",
+        age: ""
     })
     const handleChangeTextData = (e) => {
         console.log(textData)
@@ -47,27 +62,37 @@ function Signup() {
                     fullWidth
                     required
                     id="username"
-                    label="Full Nick Name"
+                    label="Full Name"
                     variant="outlined" />
             </Grid>
             <Grid item xs={12} sm={6} md={4}>
                 <p> Subjects you like</p>
                 <FormGroup >
-                    <FormControlLabel control={<Checkbox  checked={isChecked} onChange={handleCheck}/>} label="PHYSICS" />
-                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="MATHS" />
-                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="CHEMISTRY" />
-                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="ENGLISH" />
-                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="CSE" />
+                    <FormControlLabel control={<Checkbox  checked={isChecked} onChange={handleCheck}/>} label="Physics" />
+                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="Maths" />
+                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="Chemistry" />
+                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="English" />
+                    <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck}/>} label="Computer Science" />
                     
                     <FormControlLabel control={<Checkbox checked={isChecked} onChange={handleCheck} />}
                         checkedIcon={<SportsCricketIcon />} icon={<SportsCricketOutlinedIcon />}
-                        label="Sport" />
+                        label="Sports" />
                 </FormGroup>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4}>
+                <Badge badgeContent={4} color="primary">
+                    <MailIcon color="action" />
+                </Badge>
+                <br />
+                <FormControlLabel control={<Switch color='secondary' checked={isChecked} onChange={handleCheck} />}
+                    label="Wanna work?" />
+
             </Grid>
             <Grid item xs={12} md={6}>
                 <TextField
                     id="about"
-                    label="Describe Yourself"
+                    label="About Yourself"
                     multiline
                     name="about"
                     onChange={handleChangeTextData}
@@ -80,9 +105,26 @@ function Signup() {
                 />
             </Grid>
             <Grid item xs={12} md={6}>
-                
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={textData.age}
+                        label="Age"
+                        name="age"
+                        onChange={handleChangeTextData}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6}>
+
                 <FormControl>
-                    <FormLabel id="gender">GENDER</FormLabel>
+                    <FormLabel id="gender">Gender</FormLabel>
                     <RadioGroup
                         aria-labelledby="gender"
                         name="controlled-radio-buttons-group"
@@ -97,5 +139,4 @@ function Signup() {
         </Grid>
     )
 }
-
 export default Signup
